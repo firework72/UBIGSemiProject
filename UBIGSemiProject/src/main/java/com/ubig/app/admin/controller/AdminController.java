8fcc92be6398c8951239ab56d0aa048e216897d1
rlawogud02 <wogud020913@naver.com>
2025-12-18 17:08:58 +0900

    관리자 페이지 기능 일부 추가

---
+++ UBIGSemiProject/src/main/java/com/ubig/app/admin/controller/AdminController.java
@@ -0,0 +1,85 @@
+package com.ubig.app.admin.controller;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ubig.app.admin.service.AdminService;
+import com.ubig.app.vo.member.MemberVO;
+
+@Controller
+public class AdminController {
+	
+	@Autowired
+	private AdminService service;
+	
+	//관리자 페이지 이동
+	@RequestMapping("/admin")
+	public String adminPage() {
+		
+		return "admin/admin";
+	}
+	
+	//정지된 회원들 목록
+	@RequestMapping("/userStatus")
+	public String selectUser(Model model) {
+		
+		ArrayList<MemberVO> list = service.selectUser();
+		
+		model.addAttribute(list);
+		
+		return "admin/userStatus";
+	}
+	
+	//회원 정지
+	@RequestMapping("")
+	public String updateStatus(HttpSession session,String userId) {
+		
+		int result = service.updateStatus(userId);
+			
+		if(result>0) {
+			session.setAttribute("alertMsg", "회원 정지 성공");
+		}else {
+			session.setAttribute("alertMsg", "회원 정지 실패");
+		}
+		
+		return "";
+	}
+	
+	//회원 추방
+	@RequestMapping("")
+	public String deleteUser(HttpSession session,String userId) {
+		
+		int result = service.deleteUser(userId);
+		
+		if(result>0) {
+			session.setAttribute("alertMsg", "회원 추방 성공");
+		}else {
+			session.setAttribute("alertMsg", "회원 추방 성공");
+		}
+		
+		return "";
+	}
+	
+	//공지글 등록
+	@RequestMapping("")
+	public String insertBoard(HttpSession session) {
+		
+		int result = service.insertBoard();
+		
+		if(result>0) {
+			session.setAttribute("alertMsg", "공지글 등록 성공");
+		}else {
+			session.setAttribute("alertMsg", "공지글 등록 성공");
+		}
+		
+		return "";
+	}
+	
+	
+}
