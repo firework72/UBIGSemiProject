ac0fb92833a498abffbd46bf5b4edf8fca4dfd7f
firework72 <fir3work72@gmail.com>
2025-12-26 16:46:10 +0900

    Dong : 쪽지함 쪽지 페이징 처리

--- UBIGSemiProject/src/main/java/com/ubig/app/member/service/MessageService.java
+++ UBIGSemiProject/src/main/java/com/ubig/app/member/service/MessageService.java
@@ -2,18 +2,23 @@ package com.ubig.app.member.service;
 
 import java.util.ArrayList;
 
+import com.ubig.app.common.model.vo.PageInfo;
 import com.ubig.app.vo.member.MessageVO;
 
 public interface MessageService {
 
-	ArrayList<MessageVO> selectInbox(String userId);
+	ArrayList<MessageVO> selectInbox(String userId, PageInfo pi);
 
 	int unreadCount(String userId);
 
 	int insertMessage(MessageVO message);
 
-	ArrayList<MessageVO> selectSent(String userId);
+	ArrayList<MessageVO> selectSent(String userId, PageInfo pi);
 
 	int readMessage(int messageNo);
+
+	int receiveListCount(String userId);
+
+	int sendListCount(String userId);
 
 }
