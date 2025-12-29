4d246b106053da283981e3ce55246330f24dbf0a
firework72 <fir3work72@gmail.com>
2025-12-29 11:04:37 +0900

    Dong : 인터셉터에서 로그인 상태 시 세션에 로그인 정보 갱신하도록 수정

--- UBIGSemiProject/src/main/java/com/ubig/app/common/interceptor/LoginInterceptor.java
+++ UBIGSemiProject/src/main/java/com/ubig/app/common/interceptor/LoginInterceptor.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.ubig.app.vo.member.MemberVO;
+
 //로그인 해야지만 접근 가능한 페이지 인터셉터(로그인 정보가 없으면 로그인 페이지로 이동)
 public class LoginInterceptor implements HandlerInterceptor {
 
@@ -13,13 +15,16 @@ public class LoginInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 
-		Object loginMember = request.getSession().getAttribute("loginMember");
+		MemberVO loginMember = (MemberVO) request.getSession().getAttribute("loginMember");
 
 		// 로그인 정보가 없으면 로그인 페이지로 이동
 		if (loginMember == null) {
 			request.getSession().setAttribute("alertMsg", "로그인 후 접근 가능합니다.");
 			response.sendRedirect(request.getContextPath() + "/user/login.me");
 			return false;
+		}
+		else {
+			request.getSession().setAttribute("loginMember", loginMember);
 		}
 
 		return true;
