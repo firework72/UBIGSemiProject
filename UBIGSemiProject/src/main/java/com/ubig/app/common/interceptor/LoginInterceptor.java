801fd834f84ec1ab09950820e2317444a0316f31
firework72 <fir3work72@gmail.com>
2025-12-29 11:07:48 +0900

    Dong : 인터셉터에서 로그인 정보 DB를 통해 조회해오는 로직 추가

--- UBIGSemiProject/src/main/java/com/ubig/app/common/interceptor/LoginInterceptor.java
+++ UBIGSemiProject/src/main/java/com/ubig/app/common/interceptor/LoginInterceptor.java
@@ -3,12 +3,17 @@ package com.ubig.app.common.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.ubig.app.member.service.MemberService;
 import com.ubig.app.vo.member.MemberVO;
 
 //로그인 해야지만 접근 가능한 페이지 인터셉터(로그인 정보가 없으면 로그인 페이지로 이동)
 public class LoginInterceptor implements HandlerInterceptor {
+	
+	@Autowired
+	private MemberService service;
 
 	// 요청 처리 전 간섭
 	@Override
@@ -24,6 +29,8 @@ public class LoginInterceptor implements HandlerInterceptor {
 			return false;
 		}
 		else {
+			loginMember = service.loginMember(loginMember);
+			System.out.println("인터셉터에서 세션 로그인 정보를 갱신하였습니다.");
 			request.getSession().setAttribute("loginMember", loginMember);
 		}
 
